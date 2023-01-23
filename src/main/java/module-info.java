dd9839086152f3a0d51386e85ab8ea4eaebab8ac
TheD4rkCoder <david2mairhofer@gmail.com>
2023-01-23 09:15:11 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.sn_dijkstra {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.sn_dijkstra to javafx.fxml;
+    exports com.example.sn_dijkstra;
+}
