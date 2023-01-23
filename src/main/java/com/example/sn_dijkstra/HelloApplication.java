2aaba1db36d0de6ab4446c1a5a5a0239943aa3c3
TheD4rkCoder <david2mairhofer@gmail.com>
2023-01-23 09:27:18 +0100

    Added Image view

--- src/main/java/com/example/sn_dijkstra/HelloApplication.java
+++ src/main/java/com/example/sn_dijkstra/HelloApplication.java
@@ -2,16 +2,32 @@ package com.example.sn_dijkstra;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+
+    }
+    public void loadAndShowPicture(Stage stage) {
+        FileInputStream inputstream = null;
+        try {
+            inputstream = new FileInputStream("image.jpg");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Image image = new Image(inputstream);
+        ImageView imageView = new ImageView(image);
+        Scene scene = new Scene(new Group(imageView), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
