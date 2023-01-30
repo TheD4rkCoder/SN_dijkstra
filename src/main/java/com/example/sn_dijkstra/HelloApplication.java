229fba6e37dcd281ca8e64f71a973ac8ab90fb2a
TheD4rkCoder <david2mairhofer@gmail.com>
2023-01-30 09:32:23 +0100

    idk

--- src/main/java/com/example/sn_dijkstra/HelloApplication.java
+++ src/main/java/com/example/sn_dijkstra/HelloApplication.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.image.*;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
@@ -13,21 +13,41 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
+    // http://www.cse.uaa.alaska.edu/~afkjm/csce201/handouts/LoopsImages.pdf
+    static PixelReader pixelReader;
+    static PixelWriter pixelWriter;
+    static WritableImage wImage;
+    static Image image;
+
     @Override
     public void start(Stage stage) throws IOException {
-
+        loadImage();
+        showImage(stage);
 
     }
-    public void loadAndShowPicture(Stage stage) {
+
+    public void loadImage() {
         FileInputStream inputstream = null;
         try {
-            inputstream = new FileInputStream("image.jpg"); // use other file names
+            inputstream = new FileInputStream("src/main/resources/com/example/sn_dijkstra/data/IMG_1.jpg"); // use other file names
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        Image image = new Image(inputstream);
-        ImageView imageView = new ImageView(image);
-        Scene scene = new Scene(new Group(imageView), 320, 240);
+        image = new Image(inputstream);
+        pixelReader = image.getPixelReader();
+        wImage = new WritableImage((int)image.getWidth(),(int)image.getHeight());
+        PixelWriter pixelWriter = wImage.getPixelWriter();
+
+    }
+    static void showImage(Stage stage) {
+        ImageView imageView = new ImageView();
+        imageView.setImage(wImage);
+        imageView.setFitWidth(600);
+        imageView.setPreserveRatio(true);
+        Group root = new Group(imageView);
+        Scene scene = new Scene(root);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
