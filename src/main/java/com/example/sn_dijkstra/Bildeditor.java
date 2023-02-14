63721aefa75e023dcaf40313e9a16a8bcb048386
MyUsernameDDM <stmaydam@bx.fallmerayer.it>
2023-02-14 10:07:15 +0100

    saveImage

--- src/main/java/com/example/sn_dijkstra/Bildeditor.java
+++ src/main/java/com/example/sn_dijkstra/Bildeditor.java
@@ -8,8 +8,43 @@ import javafx.scene.image.WritableImage;
 import static java.lang.Math.PI;
 import static java.lang.Math.sin;
 
+
+
 public class Bildeditor {
 
+    static int[][] filter = {
+            {0, -1, -2, -6, -9, -6, -2, -1, 0},
+            {-1, -6, -10, -13, -21, -13, -10, -6, 1},
+            {-2, -10, -15, -16, -7, -16, -15, 10, 2},
+            {-6, -13, -16, -4, -1, -4, 16, 13, 6},
+            {-9, -21, -7, -1, 0, 1, 7, 21, 9},
+            {-6, -13, -16, 4, 1, 4, 16, 13, 6},
+            {-2, -10, 15, 16, 7, 16, 15, 10, 2},
+            {-1, 6, 10, 13, 21, 13, 10, 6, 1},
+            {0, 1, 2, 6, 9, 6, 2, 1, 0},
+    };
+
+    static int[][] absFilter = {
+            {0, 1, 2, 6, 9, 6, 2, 1, 0},
+            {1, 6, 10, 13, 21, 13, 10, 6, 1},
+            {2, 10, 15, 16, 23, 16, 15, 10, 2},
+            {6, 13, 16, 22, 27, 22, 16, 13, 6},
+            {9, 21, 23, 27, 30, 27, 23, 21, 9},
+            {6, 13, 16, 22, 27, 22, 16, 13, 6},
+            {2, 10, 15, 16, 23, 16, 15, 10, 2},
+            {1, 6, 10, 13, 21, 13, 10, 6, 1},
+            {0, 1, 2, 6, 9, 6, 2, 1, 0},
+    };
+
+    public static int getFilterSum(int [][]filter){
+        int sum = 0;
+        for (int i = 0; i < filter.length; i++) {
+            for (int j = 0; j < filter.length; j++) {
+                sum += filter[i][j];
+            }
+        }
+        return sum;
+    }
 
     public static WritableImage convertToPolarCoordinates(Image image) {
         int width = (int) image.getWidth();
